89efd9339e3231ad63d87ab0c0ef1f5f3a2ca2e0
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-12-19 20:53:19 +0300

    Created SearchEngine.js and imported axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Search from './Search.js';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search />
+      <footer>
+      <a href="https://github.com/Mugishaa77/shecodes-react-week-4-assignment"
+      target = "_blank"
+      rel="noreferrer">Open-source by Mugisha</a>
+    </footer>
     </div>
   );
 }
 
-export default App;
+
